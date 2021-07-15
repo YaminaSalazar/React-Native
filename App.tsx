03483f3b8870dbf7716b74ce1776d35113e1d9e2
YaminaSalazar <yaminasalazar77@gmail.com>
2021-07-15 11:37:26 -0400

    creando credenciales de login

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import {View, Image, StyleSheet, ImageBackground} from "react-native";
+import {View, Image, StyleSheet, ImageBackground, Alert} from "react-native";
 import { TextInput, Button} from "react-native-paper";
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import firebase from "./src/firebase"
+
 interface Mystate {
   username: string,
   password: string,
@@ -15,9 +18,28 @@ class App extends React.Component<any, Mystate> {
       visible: true
     }
   }
-  loginGoogle() {
-
+  onGoogleButtonPress() {
+    // Get the users ID token
+    return new Promise(async (resolve, reject) =>{
+      console.log("Login");
+      const { idToken } = await GoogleSignin.signIn();
+  
+      // Create a Google credential with the token
+      const googleCredential = firebase.getApp(). auth.GoogleAuthProvider.credential(idToken);
+      console.log(googleCredential)
+      // Sign-in the user with the credential
+      resolve(firebase.getApp().auth().signInWithCredential(googleCredential));
+    });
   }
+  componentDidMount() {
+    GoogleSignin.configure({
+      webClientId: '100177613089-29igg389oj0jubks936imqpajaijseiv.apps.googleusercontent.com',
+    });
+  }
+  async loginGoogle() {
+       var data = await this.onGoogleButtonPress();
+       console.log(data);
+  };
   render() {
     return <ImageBackground style={styles.body} source={require("./android/assets/img/Background.png.png")}>
       <View style={styles.centerObjects}> 
@@ -63,12 +85,15 @@ const styles = StyleSheet.create({
   body: {
     flex: 1
   },
+  container:{
+    justifyContent: 'center',
+    alignItem: 'center',
+  },
+
   logo:{ 
-  marginTop: "10",
   height: "60%",
   width: "60%"
   
-
 },
 containerTextfield: {
   padding: 10
@@ -76,9 +101,9 @@ containerTextfield: {
 centerObjects: {
   alignItems: "center"
 },
-/*marginTop: {
+marginTop: {
   marginTop: 10
-},*/
+},
 
 });
 export default App;
